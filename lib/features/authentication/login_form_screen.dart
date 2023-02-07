967579e7a4ea010467fc89ee9386351942969a21
oortsae <shchun519@naver.com>
2023-02-07 15:34:59 +0900

    Add login form screen & redesign directory

---
+++ lib/features/authentication/login_form_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tiktok_clone_code/features/authentication/form_button.dart';
+
+import '../../constants/gaps.dart';
+import '../../constants/sizes.dart';
+
+class LoginFormScreen extends StatefulWidget {
+  const LoginFormScreen({super.key});
+
+  @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  Map<String, String> formData = {};
+  void _onSubmitTap() {
+    if (_formkey.currentState != null) {
+      if (_formkey.currentState!.validate()) {
+        _formkey.currentState!.save();
+        print(formData);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Log in'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Gaps.v28,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['email'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['password'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v28,
+              GestureDetector(
+                  onTap: _onSubmitTap,
+                  child: const FormButton(disabled: false, text: 'Log in')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
